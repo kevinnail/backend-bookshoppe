cc851c9b8d5f33f0ddf68b56921ebe943493e7bc
kevinnail <fumalicious@gmail.com>
2022-11-08 11:16:31 -0800

    finished setup.sql, authors model and controller, updated route in app.js, adjusted test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,20 +3,18 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
-describe('/authors should return a list of authors', () => {
+describe('authors and books routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('example test - delete me!', async () => {
-    const resp = await request(app).get('/owners');
+  it('/authors should return a list of authors', async () => {
+    const resp = await request(app).get('/authors');
     expect(resp.status).toBe(200);
-    expect(resp.body.length).toBe(10);
+    expect(resp.body.length).toBe(13);
     expect(resp.body[0]).toEqual({
       id: expect.any(String),
       name: expect.any(String),
-      date_of_birth: expect.any(String),
-      place_of_birth: expect.any(String),
     });
   });
 
