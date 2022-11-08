d8fcffd751a22e6d9e3ee813f487b86695e73d71
kevinnail <fumalicious@gmail.com>
2022-11-07 22:36:33 -0800

    authors list test written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,23 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+describe('/authors should return a list of authors', () => {
   beforeEach(() => {
     return setup(pool);
   });
 
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('example test - delete me!', async () => {
+    const resp = await request(app).get('/owners');
+    expect(resp.status).toBe(200);
+    expect(resp.body.length).toBe(10);
+    expect(resp.body[0]).toEqual({
+      id: expect.any(String),
+      name: expect.any(String),
+      date_of_birth: expect.any(String),
+      place_of_birth: expect.any(String),
+    });
   });
 
   afterAll(() => {
