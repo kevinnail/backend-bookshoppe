911c38053e9f26b36e125614dd04e207da958890
kevinnail <fumalicious@gmail.com>
2022-11-08 11:38:57 -0800

    added id test

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -18,6 +18,18 @@ describe('authors and books routes', () => {
     });
   });
 
+  it('/authors/:id should return author detail and a nested list of books', async () => {
+    const resp = await request(app).get('/authors/1');
+    expect(resp.body.length).toEqual(7);
+    const george = resp.body.find((char) => char.id === '1');
+    expect(george).toHaveProperty('name', 'George J. Thompson');
+    expect(george).toHaveProperty('date_of_birth', '12/4/1940');
+    expect(george).toHaveProperty('place_of_birth', 'Seattle, WA');
+    expect(george).toHaveProperty('books');
+    expect(george.books[0]).toHaveProperty('title');
+    expect(george.books[0]).toHaveProperty('released');
+  });
+
   afterAll(() => {
     pool.end();
   });
